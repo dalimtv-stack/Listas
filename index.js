48022c10d634fba532cd96d9574eb9cc11342a09
dalimtv-stack <dalim.tv@gmail.com>
2025-08-31 13:53:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cache = new NodeCache({ stdTTL: CACHE_TTL });
 
 const manifest = {
   id: 'org.stremio.shickatacestream',
-  version: '1.0.9',
+  version: '1.1.0',
   name: 'Shickat Acestream Channels',
   description: 'Addon para cargar canales Acestream desde Shickat.me.',
   types: ['tv'],
