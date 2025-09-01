8cb195833d85f2e35d44422d80892e5e2283bbc7
dalimtv-stack <dalim.tv@gmail.com>
2025-09-01 12:12:41 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,20 @@
 // src/config.js
 module.exports = {
-  CACHE_TTL: 1800, // 30 minutes in seconds
+  // Tiempo de vida del caché (en segundos)
+  CACHE_TTL: 1800, // 30 minutos
+
+  // Puerto en el que se levantará el servidor
   DEFAULT_PORT: 3000,
-  STREAM_PREFIX: 'heimdallr_'
+
+  // Prefijo para los IDs de stream
+  STREAM_PREFIX: 'heimdallr_',
+
+  // URL de tu lista M3U (pon aquí la que estés usando)
+  M3U_URL: "https://raw.githubusercontent.com/dalimtv-stack/Listas/refs/heads/main/mi-lista.m3u",
+
+  // Nombre del addon
+  ADDON_NAME: "Heimdallr Channels",
+
+  // ID del addon (tiene que ser único)
+  ADDON_ID: "org.stremio.Heimdallr"
 };
