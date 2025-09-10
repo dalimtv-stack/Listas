18db2f0f387909af848a79785d7412d95abf9b96
dalimtv-stack <dalim.tv@gmail.com>
2025-09-10 08:33:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ loadM3U().then(() => {
 
 const manifest = {
   id: 'org.stremio.Heimdallr',
-  version: '1.2.181',
+  version: '1.2.180',
   name: 'Heimdallr Channels',
   description: 'Addon para cargar canales Acestream o M3U8 desde una lista M3U proporcionada por el usuario.',
   types: ['tv'],
