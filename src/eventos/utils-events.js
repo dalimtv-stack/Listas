276b7b251dc796074ce69cc6afe8b5723f279ebc
dalimtv-stack <dalim.tv@gmail.com>
2025-09-23 13:15:58 +0200

    Update utils-events.js

--- src/eventos/utils-events.js
+++ src/eventos/utils-events.js
@@ -3,7 +3,7 @@
 function normalizeId(ev) {
   return `eventos_${ev.dia}_${ev.hora}_${ev.deporte}_${ev.partido}`
     .replace(/\s+/g, '_')
-    .replace(/[^\w]/g, '');
+    .replace(/[^\p{L}\p{N}_]/gu, ''); // conserva letras con acentos y números
 }
 
 module.exports = { normalizeId };
