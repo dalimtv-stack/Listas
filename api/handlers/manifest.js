a70917fc0be1dc04e51097ed8b26f5a897d57d7a
dalimtv-stack <dalim.tv@gmail.com>
2025-09-23 17:03:52 +0200

    Update manifest.js

--- api/handlers/manifest.js
+++ api/handlers/manifest.js
@@ -59,8 +59,8 @@ async function buildManifest(configId) {
     resources: ['catalog', 'meta', 'stream'],
     // 👇 Aquí está la corrección: prefijos globales para canales y eventos
     idPrefixes: [
-      `${ADDON_PREFIX}_,
-      `heimdallr_evt_${configId}_`
+      `${ADDON_PREFIX}_${configId}_,
+      `Heimdallr_evt_${configId}_`
     ],
     behaviorHints: { configurable: true },
     config: [
