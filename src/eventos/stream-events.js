1adc76360a0e6641526e510b6942f2a88af29c30
dalimtv-stack <dalim.tv@gmail.com>
2025-09-24 08:11:47 +0200

    Update stream-events.js

--- src/eventos/stream-events.js
+++ src/eventos/stream-events.js
@@ -25,7 +25,7 @@ async function getStreams(id, configId) {
 
     streams.push({
       name: label, // Usar el label completo (ej. "DAZN 1 FHD --> NEW ERA")
-      title: `${evento.partido} (${evento.deporte})`,
+      title: `${evento.partido} (${evento.deporte}) (Acestream)`,
       externalUrl: url,
       behaviorHints: { notWebReady: true, external: true }
     });
