fd5711e2bdfa4c7a2f1667a470e292fa3b793fd1
dalimtv-stack <dalim.tv@gmail.com>
2025-10-08 11:12:41 +0200

    Update scraper-events.js

--- src/eventos/scraper-events.js
+++ src/eventos/scraper-events.js
@@ -295,7 +295,7 @@ async function scrapeEventosDesdeMarca(ahoraDT) {
           deporte,
           competicion,
           partido,
-          canal: canal,
+          canal,
           canales: [{ label: canal, url: null }]
         };
 
