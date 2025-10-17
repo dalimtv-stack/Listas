cf8da323f323c300ee42228f84fc956d960e4dbc
dalimtv-stack <dalim.tv@gmail.com>
2025-10-17 23:52:00 +0200

    Actualizar stream-events.js

--- src/eventos/stream-events.js
+++ src/eventos/stream-events.js
@@ -116,7 +116,7 @@ async function getStreams(id, configId) {
     'LA 1': 'LA1.es',
     'Aragón TV': 'Aragon.TV.es',
     'TVG 2': 'TVG.2.gal',
-    'M+ LaLiga': 'Movistar.LaLiga.es'
+    'M+ LaLiga': 'Movistar.LaLiga.es',
     'M+ Vamos': 'Movistar.Vamos.es'
   };
 
